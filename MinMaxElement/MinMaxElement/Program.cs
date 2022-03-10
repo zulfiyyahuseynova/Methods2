452a53eaeae1319d758504c95b31ab1b8fe3611d
unknown <tu7kswf4d@code.edu.az>
2022-03-10 12:20:14 +0400

    added Methods2

---
+++ MinMaxElement/MinMaxElement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MinMaxElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 54, -12, 5, 0, -24, 19 };
+            Swap(arr);
+            foreach (int num in arr)
+            {
+                Console.Write(num + ",");
+            }
+        }
+        static void Swap(int[] arr)
+        {
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<arr[min])
+                {
+                    min = i;
+                }
+                if (arr[i]>arr[max])
+                {
+                    max = i;
+                }
+            }
+            arr[min] = arr[max] + arr[min];
+            arr[max] = arr[min] - arr[max];
+            arr[min] = arr[min] - arr[max];
+        }
+    }
+}
---
+++ NegativeToPositive/NegativeToPositive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NegativeToPositive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, -2, 7, -9, 5, 12, -6 };
+            NegativeToPositive(ref arr);
+            foreach (int num in arr)
+            {
+                Console.Write(num + ",");
+            }
+
+        }
+        static void NegativeToPositive(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<0)
+                {
+                    arr[i] *= -1;
+                }
+            }
+        }
+    }
+}
